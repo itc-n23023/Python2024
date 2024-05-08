9d71466771bb14dcac9b49d5bdcc1d686ebdec1f
itc-n23023 <n23023@std.it-college.ac.jp>
2024-05-08 01:52:33 +0000

    first commit

---
+++ TextBook/CH01/hello.py
@@ -0,0 +1,12 @@
+# このプログラムは挨拶として表示して名前と年齢を尋ねる
+
+print('Hello, world!') #
+print('What is your name?') #名前を尋ねる
+my_name = input() #
+print('It is good to meet you, ' + my_name) #
+print('The lengthof your name is:') #
+print(len(my_name))
+print('What is toyr age?') #
+my_age = input()
+print('You will be' + str(int(my_age) + 1) + ' in a year.') #来年の年齢を表示
+
