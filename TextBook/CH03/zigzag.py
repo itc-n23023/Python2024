7c31f2790b064286b64e6ba7368a12703bbc1bfa
itc-n23023 <n23023@std.it-college.ac.jp>
2024-05-15 02:36:22 +0000

    ch03finish

---
+++ TextBook/CH03/zigzag.py
@@ -0,0 +1,26 @@
+import time, sys
+indent = 0 #インデントの幅
+indent_increasing = True #インデントが増えて売るかどう
+
+try:
+    while True: # メインのプログラムのループ
+        print(' ' * indent, end=' ')
+        print('********')
+        time.sleep(0.1) #0.1秒間止める
+
+        if indent_increasing:
+            #インデントを増やす
+            indent = indent + 1
+            if indent == 20:
+                #方向を変える
+                indent_increasing = False
+        else:
+            #インデントを減らす
+            indent = indent - 1
+            if indent == 0:
+                #方句を変える
+                indent_increasing = True
+
+except KeyboardInterrupt:
+    sys.exit()
+
